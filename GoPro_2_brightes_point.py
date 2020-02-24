f8b1b3264e90c4ba4008b47ac9c9a344b413087e
Kátia Barros <katitamendes@gmail.com>
2020-02-24 15:23:34 +0100

    Create GoPro_2_brightes_point.py

---
+++ GoPro_2_brightes_point.py
@@ -0,0 +1,24 @@
+
+# import the necessary packages
+import numpy as np
+import argparse
+import cv2
+
+# construct the argument parse and parse the arguments
+ap = argparse.ArgumentParser()
+ap.add_argument("-i", "--image", help = "path to the image file")
+ap.add_argument("-r", "--radius", type = int,
+	help = "radius of Gaussian blur; must be odd")
+args = vars(ap.parse_args())
+
+# load the image and convert it to grayscale
+image = cv2.imread(args["image"])
+orig = image.copy()
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# the area of the image with the largest intensity value
+(minVal, maxVal, minLoc, maxLoc) = cv2.minMaxLoc(gray)
+cv2.circle(image, maxLoc, 5, (255, 0, 0), 2)
+ 
+# display the results of the naive attempt
+cv2.imshow("Naive", image)
